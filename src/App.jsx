9449300d4cfdb3214a18ea8115fd6074924dd4f7
otsuko <amko22@au.com>
2025-08-22 11:11:05 +0900

    3rdcomit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState} from 'react';
+import './App.css';
+import Button from './Button';
+
+function App() {
+
+const [ count, setCount ]= useState(0)
+
+  return (
+    <div className="App">
+      <p>カウント: {count}</p>
+     <Button 
+     btn_click={()=> { setCount(count + 1)}}  
+     btn_txt='1増やす'
+     />
+
+     <Button
+          btn_click={()=> { setCount(count - 1)}}  
+     btn_txt='1減らす'
+     />
+
+  
+    </div>
+  );
+}
+
+
+
+
+export default App;
